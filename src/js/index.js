d526b9d0745cfe72748ca0a07141b5223d5beedc
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-02-15 18:12:32 +0200

    fixed

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Notiflix from 'notiflix';
 import '../css/styles.css';
 import { fetchCountry } from './fetchCountries';
+import {debounce} from 'lodash';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -9,7 +10,9 @@ const countryInfoDOM = document.querySelector('.country-info')
 
 let input = document.querySelector('input')
 
-input.addEventListener('input', (event)=>{
+console.log(213123123);
+
+input.addEventListener('input', debounce((event)=>{
     let countryInfo ='', countryList ='';
     if(event.target.value === ''){
         countryListDOM.innerHTML =''
@@ -19,6 +22,8 @@ input.addEventListener('input', (event)=>{
     fetchCountry(event.target.value)
         .then((data) => {
             if(data.message != undefined){
+                countryInfoDOM.innerHTML = '';
+                countryListDOM.innerHTML = ''
                 Notiflix.Notify.failure('Oops, there is no country with that name.')
             }
             if(data.length === 1){
@@ -33,7 +38,7 @@ input.addEventListener('input', (event)=>{
                                 <p class ='languages info-item'><b>Languages:</b> ${languagesStr}</p>`;
                 countryInfoDOM.innerHTML = countryInfo
             }
-            if(data.length > 1 && data.length <= 15){
+            if(data.length > 1 && data.length <= 10){
                 countryInfoDOM.innerHTML = '';
                 const countrysNames =  data.flatMap((country) => country.name.official)
                 countrysNames.forEach((el, index) => {
@@ -44,9 +49,11 @@ input.addEventListener('input', (event)=>{
                 countryListDOM.innerHTML = countryList
             }
             if(data.length > 15){
+                countryInfoDOM.innerHTML = '';
+                countryListDOM.innerHTML = ''
                 Notiflix.Notify.info('Too many matchese found. Please enter a more specific name.')
             }
             
         })
     }
-})
+}, DEBOUNCE_DELAY))
